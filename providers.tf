cf00c771e8f58bfc077969c4d191124fd3d35c1c
Alex Billingsley <abillingsley@developertown.com>
2023-05-24 13:22:50 -0400

    cleanup

--- providers.tf
+++ providers.tf
@@ -21,15 +21,3 @@ provider "aws" {
     tags = var.tags
   }
 }
-
-provider "aws" {
-  region = var.region
-  alias  = "apex_dns_zone_account"
-
-  assume_role {
-    role_arn = var.parent_role_arn
-  }
-  default_tags {
-    tags = var.tags
-  }
-}
